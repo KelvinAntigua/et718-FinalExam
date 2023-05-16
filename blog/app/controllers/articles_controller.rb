8805ef944ec698798e76c6d224da0f1ec8c84b11
EC2 Default User <ec2-user@ip-10-16-0-37.ec2.internal>
2023-05-16 17:01:52 +0000

    finished exam

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  
+   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
 def index
     @articles = Article.all
@@ -11,6 +13,10 @@ end
 def new
   @article = Article.new
 end
+
+def edit
+  @article = Article.find(params[:id])
+end
  
 def create
   @article = Article.new(article_params)
@@ -21,6 +27,26 @@ def create
     render 'new'
   end
 end
+
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
+
+
+
  
 private
   def article_params
